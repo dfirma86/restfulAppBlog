e836b9d778fed7af609661b872069ea1ad292b6e
Dino Firmalino <dfirma86@yahoo.com>
2019-05-22 13:59:42 -0700

    git add Semantic UI nav bar

--- app.js
+++ app.js
@@ -4,24 +4,17 @@ const express = require('express'),
   mongoose = require('mongoose')
 
 // APP CONFIG
-mongoose.connect('mongodb://localhost/restful_blog_app', {
-  useNewUrlParser: true
-})
+mongoose.connect('mongodb://localhost/restful_blog_app', { useNewUrlParser: true })
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // MONGOOSE MODEL CONFIG
 const blogSchema = new mongoose.Schema({
   title: String,
   image: String,
   body: String,
-  created: {
-    type: Date,
-    default: Date.now
-  }
+  created: { type: Date, default: Date.now }
 })
 
 const Blog = mongoose.model('Blog', blogSchema)
@@ -46,11 +39,9 @@ app.get('/blogs', (req, res) => {
     if (err) {
       console.log(err)
     } else {
-      res.render('index', {
-        blogs: blogs
-      })
+      res.render('index', { blogs: blogs })
     }
   })
 })
 
-app.listen(3000, () => console.log('Running!'))
+app.listen(3000, () => console.log('Server is live!'))
