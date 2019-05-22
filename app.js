4321a07cfd0b4b6c0eed7da7d0b49032f424e352
Dino Firmalino <dfirma86@yahoo.com>
2019-05-21 19:38:01 -0700

    Create Blog model

--- app.js
+++ app.js
@@ -3,15 +3,54 @@ const express = require('express'),
   bodyParser = require('body-parser'),
   mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/restful_blog_app')
+// APP CONFIG
+mongoose.connect('mongodb://localhost/restful_blog_app', {
+  useNewUrlParser: true
+})
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({
   extended: true
 }))
+
+// MONGOOSE MODEL CONFIG
+const blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
 // title
 // image url
 // body
 // created
 
-app.listen(PORT, () => console.log(`Running on ${PORT}`))
+// Blog.create({
+//   title: 'test blog',
+//   image: 'https://www.what-dog.net/Images/faces2/scroll001.jpg',
+//   body: 'this is a test'
+// })
+
+// RESTFUL ROUTES
+app.get('/', (req, res) => {
+  res.redirect('blogs')
+})
+
+app.get('/blogs', (req, res) => {
+  Blog.find({}, (err, blogs) => {
+    if (err) {
+      console.log(err)
+    } else {
+      res.render('index', {
+        blogs: blogs
+      })
+    }
+  })
+})
+
+app.listen(3000, () => console.log('Running!'))
