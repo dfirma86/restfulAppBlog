7df8ef1c8f8d29f4e1fbd4eb83945eaa030a7a55
Dino Firmalino <dfirma86@yahoo.com>
2019-05-22 20:04:41 -0700

    add functionality to delete blog post

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const express = require('express'),
   methodOverride = require('method-override')
 
 // APP CONFIG
-mongoose.connect('mongodb://localhost/restful_blog_app', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/restful_blog_app', {
+  useNewUrlParser: true,
+  useFindAndModify: false
+})
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -81,7 +84,20 @@ app.put('/blogs/:id', (req, res) => {
     if (err) {
       res.redirect('/blogs')
     } else {
-      res.redirect(`/blogs/` + req.params.id)
+      res.redirect(`/blogs/${req.params.id}`)
+    }
+  })
+})
+
+// DELETE ROUTE
+app.delete('/blogs/:id', (req, res) => {
+  // destroy blog
+  // redirect somewhere
+  Blog.findByIdAndRemove(req.params.id, err => {
+    if (err) {
+      res.redirect('/blogs')
+    } else {
+      res.redirect('/blogs')
     }
   })
 })
