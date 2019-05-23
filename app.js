a30576055460a885efb796e99dc23bb5df38a49b
Dino Firmalino <dfirma86@yahoo.com>
2019-05-22 17:00:24 -0700

    add functionality to edit and update a blog post

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express'),
   app = express(),
   bodyParser = require('body-parser'),
-  mongoose = require('mongoose')
+  mongoose = require('mongoose'),
+  methodOverride = require('method-override')
 
 // APP CONFIG
 mongoose.connect('mongodb://localhost/restful_blog_app', { useNewUrlParser: true })
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 // MONGOOSE MODEL CONFIG
 const blogSchema = new mongoose.Schema({
@@ -58,6 +60,28 @@ app.get('/blogs/:id', (req, res) => {
       res.redirect('/blogs')
     } else {
       res.render('show', { blog: foundBlog })
+    }
+  })
+})
+
+// EDIT ROUTE
+app.get('/blogs/:id/edit', (req, res) => {
+  Blog.findById(req.params.id, (err, foundBlog) => {
+    if (err) {
+      res.redirect('/blogs')
+    } else {
+      res.render('edit', { blog: foundBlog })
+    }
+  })
+})
+
+// UPDATE ROUTE
+app.put('/blogs/:id', (req, res) => {
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog) => {
+    if (err) {
+      res.redirect('/blogs')
+    } else {
+      res.redirect(`/blogs/` + req.params.id)
     }
   })
 })
